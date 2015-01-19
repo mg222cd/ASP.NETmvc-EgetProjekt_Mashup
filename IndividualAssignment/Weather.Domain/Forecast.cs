ef324671af3b8b9518795e12ca3e06148a2428f4
Marike Grinde <mg222cd@student.lnu.se>
2015-01-19 22:54:50 +0100

    EF-anslutning och databas-klasser

---
+++ IndividualAssignment/Weather.Domain/Forecast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Domain
+{
+    public partial class Forecast
+    {
+        public Forecast()
+        {
+            // Empty!
+        }
+
+        public Forecast(DateTime lastUpdate, DateTime nextUpdate, DateTime timefrom, DateTime timeto, int timeperiod, int temperature, string symbolId, Geoname geoname)
+        {
+            lastupdate = lastUpdate;
+            nextupdate = nextUpdate;
+            timeFrom = timefrom;
+            timeTo = timeto;
+            period = timeperiod;
+            temp = temperature;
+            symbol = symbolId;
+            geoId = geoname.geoId;
+            geoname.nextUpdate = nextupdate;
+            Geoname = geoname;
+        }
+    }
+}
---
+++ IndividualAssignment/Weather.Domain/Geoname.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Domain
+{
+    public partial class Geoname
+    {
+        public Geoname(string id, string cityName, string region, string country, string latitude, string langitude)
+            : this()
+        {
+            geonameId = id;
+            name = cityName;
+            adminName1 = region;
+            countryName = country;
+            lat = latitude;
+            lng = langitude;
+        }
+    }
+}
