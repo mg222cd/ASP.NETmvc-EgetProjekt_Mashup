afd452795941ad8b703c9eea81246c00fff89a2d
Marike Grinde <mg222cd@student.lnu.se>
2015-01-20 08:21:11 +0100

    Lagt in vyer och CSS

--- IndividualAssignment/Weather.MVC/App_Start/RouteConfig.cs
+++ IndividualAssignment/Weather.MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Weather.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Weather", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
