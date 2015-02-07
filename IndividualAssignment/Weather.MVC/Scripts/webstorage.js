69dc6477ca994e293c31d6ce5d2eee45d4b527c5
Marike Grinde <mg222cd@student.lnu.se>
2015-02-07 18:01:59 +0100

    Revert "testcode"
    This reverts commit 4a8992215555bbab4f075c97607dc49dca3c25e3.

--- IndividualAssignment/Weather.MVC/Scripts/webstorage.js
+++ IndividualAssignment/Weather.MVC/Scripts/webstorage.js
@@ -12,34 +12,6 @@ function checkConnection() {
 }
 
 function storeData() {
-        /*
-        // Hämtar ut innehållet i local storage variabeln 
-        var storedData = localStorage.getItem("searches");
-        var storedSearch = JSON.parse(storedData);
-
-        // Om det finns något i variabeln...
-        if (storedSearch != null) {
-
-            // Om sökningen redan finns i arrayen tas den gamla bort
-            if ($.inArray(search, storedSearch) !== -1) {
-                storedSearch.splice($.inArray(search, storedSearch), 1);
-            }
-            // Tar bort den äldsta sökningen om det finns fler än 10 element
-            if (storedSearch.length > 10) {
-                storedSearch.shift();
-            }
-
-            // Lägger till sökningen i strängen
-            storedSearch.push(search);
-            localStorage['searches'] = JSON.stringify(storedSearch);
-        }
-            // ...annars skapas en helt ny array som prognoserna läggs till i.
-        else {
-            var newArray = [];
-            newArray.push(search);
-            localStorage['searches'] = JSON.stringify(newArray);
-        }
-        */
-
+    alert('STORE DATA!');
 }
 
