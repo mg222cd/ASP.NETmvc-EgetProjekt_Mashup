1f87eba14c689ea054a0164bcd6bfe6bd36f8cb6
Marike Grinde <mg222cd@student.lnu.se>
2015-02-07 13:36:43 +0100

    fix: testing app cache

--- IndividualAssignment/Weather.MVC/Scripts/webstorage.js
+++ IndividualAssignment/Weather.MVC/Scripts/webstorage.js
@@ -7,39 +7,54 @@ function checkConnection() {
         this.storeData();
     }
     else {
-        //TODO
+        this.offlineFunction();
     }
 }
 
-function storeData() {
-        /*
-        // Hämtar ut innehållet i local storage variabeln 
-        var storedData = localStorage.getItem("searches");
-        var storedSearch = JSON.parse(storedData);
-
-        // Om det finns något i variabeln...
-        if (storedSearch != null) {
-
-            // Om sökningen redan finns i arrayen tas den gamla bort
-            if ($.inArray(search, storedSearch) !== -1) {
-                storedSearch.splice($.inArray(search, storedSearch), 1);
-            }
-            // Tar bort den äldsta sökningen om det finns fler än 10 element
-            if (storedSearch.length > 10) {
-                storedSearch.shift();
-            }
-
-            // Lägger till sökningen i strängen
-            storedSearch.push(search);
-            localStorage['searches'] = JSON.stringify(storedSearch);
-        }
-            // ...annars skapas en helt ny array som prognoserna läggs till i.
-        else {
-            var newArray = [];
-            newArray.push(search);
-            localStorage['searches'] = JSON.stringify(newArray);
-        }
-        */
-
+function offlineFunction() {
 }
+
+function storeData() {
+    /*
+    //H3 Mainheader
+    var forecastHeader = document.getElementById("forecastHeader");
+    //Statustext
+    var forecastUpdated = document.getElementById("forecastUpdated");
+    //Forecast headers (weekdays)
+    var table = document.getElementsByTagName("td");
+    console.log(table[0].innerHTML);
+    for (var i = 0; i < table.length; i++) {
+        //
+    }
+    */
+}
+
+/*
+// Hämtar ut innehållet i local storage variabeln 
+var storedData = localStorage.getItem("searches");
+var storedSearch = JSON.parse(storedData);
+
+// Om det finns något i variabeln...
+if (storedSearch != null) {
+
+    // Om sökningen redan finns i arrayen tas den gamla bort
+    if ($.inArray(search, storedSearch) !== -1) {
+        storedSearch.splice($.inArray(search, storedSearch), 1);
+    }
+    // Tar bort den äldsta sökningen om det finns fler än 10 element
+    if (storedSearch.length > 10) {
+        storedSearch.shift();
+    }
+
+    // Lägger till sökningen i strängen
+    storedSearch.push(search);
+    localStorage['searches'] = JSON.stringify(storedSearch);
+}
+    // ...annars skapas en helt ny array som prognoserna läggs till i.
+else {
+    var newArray = [];
+    newArray.push(search);
+    localStorage['searches'] = JSON.stringify(newArray);
+}
+*/
 
